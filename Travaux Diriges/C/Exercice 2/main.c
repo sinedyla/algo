f61e07ab1408d18181f671da6c8d379d4946f45e
Sinedyla <sinedyla@gmail.com>
2025-05-28 01:53:50 +0000

    Somme de deux entiers

---
+++ Travaux Diriges/C/Exercice 2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, somme;
+    printf("Entrez le premier entier : ");
+    scanf("%d", &a);
+    printf("Entrez le premier entier : ");
+    scanf("%d", &b);
+
+    printf("La somme est %d", a + b);
+
+    return 0;
+}
