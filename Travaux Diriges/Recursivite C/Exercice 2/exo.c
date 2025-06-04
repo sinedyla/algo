ff69a36cf397e6cc845afe12a403d4b28b58b321
Sinedyla <sinedyla@gmail.com>
2025-06-04 23:25:59 +0000

    Suite de Fibonacci

---
+++ Travaux Diriges/Recursivite C/Exercice 2/exo.c
@@ -0,0 +1,21 @@
+// Suite de Fibonacci
+
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n;
+    printf("Entrez un entier positif: ");
+    scanf("%d", &n);
+    printf("Le terme %d de la suite de Fibonacci est: %d\n", n, fibonacci(n));
+    return 0;
+}
