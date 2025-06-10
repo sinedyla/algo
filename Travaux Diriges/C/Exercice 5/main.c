b9bc5c2f89e5bfd59d1684a315938032972271c9
Sinedyla <sinedyla@gmail.com>
2025-06-10 23:50:13 +0000

    PGCD

---
+++ Travaux Diriges/C/Exercice 5/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main() {
+    int a, b, temp;
+    int x,y;
+
+    printf("Entrez le premier nombre: ");
+    scanf("%d", &a);
+    printf("Entrez le second nombre: ");
+    scanf("%d", &b);
+
+    x = a;
+    y = b;
+    while (y != 0) {
+        temp = y;
+        y = x % y;
+        x = temp;
+    }
+    printf("PGCD(%d,%d) = %d\n", a, b, x);
+
+    return 0;
+}
