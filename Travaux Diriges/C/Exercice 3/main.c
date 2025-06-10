09e26000b9b712e0f7694fcda6bb8e174945f03f
Sinedyla <sinedyla@gmail.com>
2025-06-10 23:44:10 +0000

    Table de multiplication

---
+++ Travaux Diriges/C/Exercice 3/main.c
@@ -0,0 +1,14 @@
+﻿// Écrire un programme en C qui affiche la table de multiplication de n. En utilisant la boucle "do-while".
+#include <stdio.h>
+int main() {
+    int n, i = 1;
+    printf("Entrez un nombre: ");
+    scanf("%d", &n);
+    printf("Table de multiplication de %d : \n", n);
+    do {
+        printf("%d x %d = %d\n", n, i, n * i);
+        i++;
+    } while (i <= 10);
+    
+    return 0;
+}
