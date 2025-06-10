c06359e3ff3065836a4864441db5c1b10603e2fc
Sinedyla <sinedyla@gmail.com>
2025-06-10 23:46:45 +0000

    Conversion nombre de jours en jours, annees, semaines...

---
+++ Travaux Diriges/C/Exercice 4/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+    int jours, annees, semaines, jours_restants;
+
+    printf("Entrez le nombre de jours: ");
+    scanf("%d", &jours);
+
+    annees = jours / 365;
+    jours_restants = jours % 365;
+    semaines = jours_restants / 7;
+    jours_restants = jours_restants % 7;
+
+    printf("%d jours = %d annees, %d semaines et %d jours\n", jours, annees, semaines, jours_restants);
+
+    return 0;
+}
