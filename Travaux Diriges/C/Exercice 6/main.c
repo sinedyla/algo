e8954b36de0c6489f70cd42845cb964a5ee6cc33
Sinedyla <sinedyla@gmail.com>
2025-06-11 23:03:29 +0000

    La distance entre deux points

---
+++ Travaux Diriges/C/Exercice 6/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float x1, y1, x2, y2, distance;
+    printf("Entrez les coordonnées du premier point (x1, y1) : ");
+    scanf("%f %f", &x1, &y1);
+    printf("Entrez les coordonnées du deuxième point (x2, y2) : ");
+    scanf("%f %f", &x2, &y2);
+
+    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("La distance entre les deux points est : %.2f\n", distance);
+
+    return 0;
+}
