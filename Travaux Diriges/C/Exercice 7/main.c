2a862707e48d881bad3a7efc3fb87b80c11a0acd
Sinedyla <sinedyla@gmail.com>
2025-06-11 23:07:09 +0000

    Nombre inversé

---
+++ Travaux Diriges/C/Exercice 7/main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main() {
+    int nombre, inverse = 0, reste;
+
+    printf("Entrez un nombre entier : ");
+    scanf("%d", &nombre);
+    while (nombre != 0) {
+        reste = nombre % 10;
+        inverse = inverse * 10 + reste;
+        nombre /= 10;
+    }
+    printf("Le nombre inversé est : %d\n", inverse);
+    return 0;
+}
