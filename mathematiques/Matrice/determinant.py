498e51f738a91711dca958a9f327fb66da8873bf
Sinedyla <sinedyla@gmail.com>
2025-06-13 21:19:45 +0000

    Determinant d'une matrice

---
+++ mathematiques/Matrice/determinant.py
@@ -0,0 +1,18 @@
+﻿import numpy as np
+
+def determinant(matrice):
+    """
+    Calcule le déterminant d'une matrice carrée.
+    """
+
+    if matrice.ndim != 2 or matrice.shape[0] != matrice.shape[1]:
+        raise ValueError("La matrice doit être carrée.")
+
+    return np.linalg.det(matrice)
+
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    matrice = np.array([[1, 2], [3, 4]])
+    det = determinant(matrice)
+    print(f"Le déterminant de la matrice est : {det}")
