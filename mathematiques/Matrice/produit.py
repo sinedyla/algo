51e55b6fbf5ae214a4763be94a28fb369683e7d9
Sinedyla <sinedyla@gmail.com>
2025-06-13 23:23:53 +0000

    Produit de deux matrices

---
+++ mathematiques/Matrice/produit.py
@@ -0,0 +1,20 @@
+﻿"""
+Produit de deux matrices.
+"""
+
+import numpy as np
+
+def produit_matrice(A, B):
+    return np.dot(A, B)
+
+
+
+
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    A = np.array([[1, 2], [3, 4]])
+    B = np.array([[5, 6], [7, 8]])
+    C = produit_matrice(A, B)
+    print("Produit de A et B : ")
+    print(C)
