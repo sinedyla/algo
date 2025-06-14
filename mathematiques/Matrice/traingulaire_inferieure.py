3b3b0238ab65b88e33b0d9cd13efe18744592aab
Sinedyla <sinedyla@gmail.com>
2025-06-14 23:16:06 +0000

    Matrice triangulaire inférieure

---
+++ mathematiques/Matrice/traingulaire_inferieure.py
@@ -0,0 +1,25 @@
+﻿import numpy as np
+def est_triangulaire_inferieure(matrice):
+    """
+    Vérifie si une matrice est triangulaire inférieure.
+    Une matrice est triangulaire inférieure si tous les éléments au dessus de la diagonale principale sont nuls.
+    """
+    mat = np.array(matrice)
+    # Vérifier si tous les éléments au-dessus de la diagonale principale sont nuls
+    return np.all(np.triu(mat, k=1) == 0)
+
+
+
+
+matrice = [[1, 0, 0],
+           [4, 2, 0],
+           [7, 8, 3]]
+print(est_triangulaire_inferieure(matrice))
+
+matrice_tri = [[1, 0, 0],
+               [4, 2, 0],
+               [7, 8, 3]]
+print(est_triangulaire_inferieure(matrice_tri))  
+
+
+
