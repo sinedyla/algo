737a4d58c19ff373ea31b364318d599618154995
Sinedyla <sinedyla@gmail.com>
2025-06-14 23:24:32 +0000

    Matrice Superieure

---
+++ mathematiques/Matrice/triangulaire_superieur.py
@@ -0,0 +1,11 @@
+﻿import numpy as np
+def est_triangulaire_superieur(matrice):
+    mat = np.array(matrice)
+    return np.all(np.tril(mat, k=-1) == 0)
+
+
+
+matrice = [[1, 2, 3],
+           [0, 5, 6],
+           [0, 0, 9]]
+print(est_triangulaire_superieur(matrice))
