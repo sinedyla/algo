a67f0e674b9d654dfaa20368a5270276be4af38a
Sinedyla <sinedyla@gmail.com>
2025-06-15 23:49:49 +0000

    Nouveau programme

---
+++ mathematiques/Autres/Crible d'Eratosthene/main.py
@@ -0,0 +1,13 @@
+﻿def crible_eratosthene(n):
+    premiers = []
+    for i in range(2, n):
+        est_premier = True
+        for p in premiers:
+            if p * p > i:
+                break
+            if i % p == 0:
+                est_premier = False
+                break
+        if est_premier:
+            premiers.append(i)
+    return premiers
