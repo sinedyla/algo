ba39359fc3edaa763658accf3a138f38475d9932
Sinedyla <sinedyla@gmail.com>
2025-06-15 23:53:21 +0000

    Nouveau programme

---
+++ mathematiques/Autres/Triangle de Pascal/main.py
@@ -0,0 +1,9 @@
+﻿def triangle_de_pascal(n):
+    triangle = []
+    for i in range(n):
+        ligne = [1] * (i + 1)
+        for j in range(1, i):
+            ligne[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(ligne)
+    return triangle
+
