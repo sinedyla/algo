c2cee3784c4a22ca1e46994a2f3913be37ba3fb1
Sinedyla <sinedyla@gmail.com>
2025-06-16 23:14:05 +0000

    puissance_modulaire_rapide

---
+++ mathematiques/Autres/Puissance modulaire rapide/main.py
@@ -0,0 +1,10 @@
+﻿def puissance_modulaire_rapide(a, n, p) :
+    result = 1
+    a = a % p
+    while n > 0:
+        if (n % 2) == 1 :
+            result = (result * a) % p
+        n = n // 2
+        a = (a * a) % p
+    return result
+
