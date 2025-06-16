228a8f33b51293f859a4339fd2d026bae12bcafe
Sinedyla <sinedyla@gmail.com>
2025-06-16 23:01:36 +0000

    Nombres amicaux

---
+++ mathematiques/Autres/Nombres amicaux/main.py
@@ -0,0 +1,14 @@
+﻿# Deux nombres sont amicaux si la somme des diviseurs propres de l’un est egale à l’autre (et vice-versa).
+
+
+def somme_diviseurs_propres(n):
+    if n < 2:
+        return 0
+    somme = 1
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            somme += i
+            if i != n // i :
+                somme += n // i
+    return somme
+
