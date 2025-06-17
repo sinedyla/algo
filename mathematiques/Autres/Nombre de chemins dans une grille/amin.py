26eec683ce2d69b03102ac8ff8cdae7b4f92f2e4
Sinedyla <sinedyla@gmail.com>
2025-06-17 23:14:55 +0000

    Inverses modulo

---
+++ mathematiques/Autres/Nombre de chemins dans une grille/amin.py
@@ -0,0 +1,13 @@
+﻿# Nombre de chemins dans une grille
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+
+
+def chemins(n, m):
+    return fact(n + m) // (fact(n) * fact(m))
+
+
