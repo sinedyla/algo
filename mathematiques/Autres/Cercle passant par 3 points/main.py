06c1121909f5f92f627bec4640ec6dfcf3d07b01
Sinedyla <sinedyla@gmail.com>
2025-06-18 23:14:29 +0000

    Cercle passant par 3 points

---
+++ mathematiques/Autres/Cercle passant par 3 points/main.py
@@ -0,0 +1,17 @@
+﻿import numpy as np
+
+
+def cercle_passant_par_3_points(p1, p2, p3):
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+
+    A = np.array([[x1 - x2, y1 - y2],
+                  [x1 - x3, y1 - y3]])
+    B = np.array([0.5 * ((x1**2 - x2**2) + (y1**2 - y2**2)),
+                  0.5 * ((x1**2 - x3**2) + (y1**2 - y3**2))])
+
+    cx, cy = np.linalg.solve(A, B)
+    r = np.sqrt((cx - x1)**2 + (cy - y1)**2)
+
+    return (cx, cy), r
