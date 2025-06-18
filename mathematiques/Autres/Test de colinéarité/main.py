f847230394a51026b1d57bd8341136392d25ed78
Sinedyla <sinedyla@gmail.com>
2025-06-18 23:12:19 +0000

    Test de colinéarité

---
+++ mathematiques/Autres/Test de colinéarité/main.py
@@ -0,0 +1,19 @@
+﻿"""
+Test de colinéarité
+érifie si trois points (x1, y1), (x2, y2), (x3, y3) sont alignés.
+"""
+
+def colineaires(x1, y1, x2, y2, x3, y3):
+    aire = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+    return aire == 0
+
+
+
+# Autre methode utilisant le produit vectoriel
+
+def colineaires_vectoriel(x1, y1, x2, y2, x3, y3) :
+    v1x = x2 - x1
+    v1y = y2 - y1
+    v2x = x3 - x1
+    v2y = y3 - y1
+    return v1x * v2y == v1y * v2x
