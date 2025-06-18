af8d483cd21eab96e9b5b07b3d52b7aed167aa65
Sinedyla <sinedyla@gmail.com>
2025-06-18 23:09:25 +0000

    Calcul de laire dun polygone

---
+++ mathematiques/Autres/Calcul de l’aire d’un polygone/main.py
@@ -0,0 +1,14 @@
+﻿def aire_polygone(coordonnees) :
+    n = len(coordonnees)
+    if n < 3:
+        raise ValueError("Un polygone doit avoir au moins 3 sommets")
+    
+    aire = 0
+    for i in range(n) :
+        x1, y1 = coordonnees[i]
+        x2, y2 = coordonnees[(i + 1) % n]
+        aire += x1 * y2 - x2 * y1
+    
+    return abs(aire) / 2
+
+
