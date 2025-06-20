3f32f86f4f2f9ceb462ad2d6d588e90682a7c7ff
Sinedyla <sinedyla@gmail.com>
2025-06-20 23:06:43 +0000

    Pile ou Face

---
+++ mathematiques/Proba/Pile ou face/main.py
@@ -0,0 +1,10 @@
+﻿import random
+def lancer_piece():
+    return random.choice(['face', 'pile'])
+
+def simuler_lancers(n):
+    resultats = {'face': 0, 'pile': 0}
+    for _ in range(n):
+        resultat = lancer_piece()
+        resultats[resultat] += 1
+    return resultats
