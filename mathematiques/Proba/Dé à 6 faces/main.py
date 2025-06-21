2cba5c876ec04e083387a1e5f6b628d3cf95c108
Sinedyla <sinedyla@gmail.com>
2025-06-21 23:44:30 +0000

    Simuler lancer dé

--- mathematiques/Proba/Dé à 6 faces/main.py
+++ mathematiques/Proba/Dé à 6 faces/main.py
@@ -2,3 +2,11 @@
 
 def lancer_de():
     return random.randint(1, 6)
+
+
+def simuler_lancers(n):
+    resultats = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    for _ in range(n):
+        resultat = lancer_de()
+        resultats[resultat] += 1
+    return resultats
